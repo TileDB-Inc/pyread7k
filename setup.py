3c6ca71822ed97cfe13b196f073af52f370f3511
Rasmus Klett Mortensen <rasmus.mortensen@teledyne.com>
2020-11-03 15:48:32 +0100

    Add setup.py package definition

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyread7k",
+    version="0.0.1",
+    author="Rasmus Klett Mortensen",
+    author_email="rasmus.mortensen@teledyne.com",
+    description="Reading 7k files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://rot-bitbucket/projects/CB7123/repos/pyread7k",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
