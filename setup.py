30766529abad155be249ccafcc5197225af66cd4
Rógvi Dávid Arge <rda@flowtale.ai>
2021-03-09 12:59:44 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyread7k",
-    version="0.0.2",
+    version="0.0.3",
     author="Rasmus Klett Mortensen",
     author_email="rasmus.mortensen@teledyne.com",
     description="Reading 7k files",
