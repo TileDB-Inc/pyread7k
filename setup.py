52540fa520da5126a0e5dbe6158fab38f1efa875
Rasmus Klett Mortensen <rasmus.mortensen@teledyne.com>
2021-03-18 09:58:41 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyread7k",
-    version="0.0.3",
+    version="1.0.0",
     author="Rasmus Klett Mortensen",
     author_email="rasmus.mortensen@teledyne.com",
     description="Reading 7k files",
