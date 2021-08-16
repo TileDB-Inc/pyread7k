523dcac48d685369c52d987d4bceda571a1da40d
George Sakkis <george.sakkis@tiledb.com>
2021-08-16 22:49:56 +0300

    Refactor _ping module
    - Rename Manager7k to S7KFileReader and extract a S7KReader base abstract class from it
      - Move the bulk of the PingDataset logic to a S7KReader.iter_pings() method
      - Change file_catalog from attribute to a cached property
      - Add file_header cached property
      - Rename get_configuration_record to configuration and make it a cached_property
      - Simplify get_records_during_ping
    - Internal Ping refactoring
    - Replace np.searchsorted with bisect
    - Drop LazyMap
    - Add missing typing annotations and casts

--- pyread7k/__init__.py
+++ pyread7k/__init__.py
@@ -1,4 +1,4 @@
 from ._datablock import DRFBlock
 from ._datarecord import record
-from ._ping import ConcatDataset, Ping, PingDataset, PingType
+from ._ping import ConcatDataset, Ping, PingDataset, PingType, S7KFileReader, S7KReader
 from ._utils import *
